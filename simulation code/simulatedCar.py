b77ce1dc0ae67246258883a5ebeb35970987cb97
Thijs van Liempd <33570639+thijses@users.noreply.github.com>
2021-03-31 23:32:23 +0200

    added docstrings and updates from testing

--- simulation code/simulatedCar.py
+++ simulation code/simulatedCar.py
@@ -9,6 +9,8 @@ from Map import Map
 
 
 class simCar(Map.Car):
+    """a class that simulates the car
+        (replace Map.Car object with this)"""
     def __init__(self):
         Map.Car.__init__(self) #init Car object
         
@@ -17,18 +19,21 @@ class simCar(Map.Car):
         
         self.min_velocity = -1
         self.max_velocity = 3
-        self.min_steering = np.deg2rad(-25)
-        self.max_steering = np.deg2rad(25)
+        self.maxSteeringAngle = np.deg2rad(25) #maybe this should be Map.Car?
         
         self.lastSimUpdateTime = time.time()
     
     def sendSpeedAngle(self, speed, angle):
+        """set desired speed and steering angle
+            (fake version of carMCU class)"""
         ## if you wanted to simulate overshoot, or steering change velocity/acceleration, here is the place to do it
         ## for now, i'm lazy and i dont know how the real car handles anyway, so we'll just keep it super simple:
         self.desired_velocity = speed
         self.desired_steering = angle
     
     def getFeedback(self):
+        """handle acceleration (both velocity and steering), to simulate getting sensor feedback from car
+            (fake version of carMCU class)"""
         ## in the realCar class, speed and steering is updated in car.update(), but for the simulation we'll just update those here
         rightNow = time.time()
         dt = rightNow - self.lastSimUpdateTime
@@ -45,7 +50,7 @@ class simCar(Map.Car):
                 self.steering = self.steering + (self.steer_accel * dt)
             else:
                 self.steering = self.steering - (self.steer_accel * dt)
-        #self.steering = min(max(self.steering, self.min_steering), self.max_steering)
+        #self.steering = min(max(self.steering, -self.maxSteeringAngle), self.maxSteeringAngle)
         
         self.lastSimUpdateTime = rightNow
         
