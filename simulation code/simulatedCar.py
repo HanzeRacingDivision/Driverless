aac4354fa493789e0408a6360f14539d786f19a9
Thijs van Liempd <33570639+thijses@users.noreply.github.com>
2021-04-06 10:10:46 +0200

    added Map.clock compatibility
    see recent Map.py update

--- simulation code/simulatedCar.py
+++ simulation code/simulatedCar.py
@@ -1,18 +1,20 @@
 
-import time
-import numpy as np  #general math library
 
 from Map import Map
 
 
+#this shouldn't be here (because this is intended only for CAR simulation code), but there's no better place for it (YET)
+import time
+def simClockExample(self): #usage: 'mapObject.setClock(simClockExample)'  now, when mapObject.clock() is called, simClockExample(mapObject) will be called
+    return((time.time()-self.clockStart)*2)
 
 
 
 class simCar(Map.Car):
     """a class that simulates the car
         (replace Map.Car object with this)"""
-    def __init__(self):
-        Map.Car.__init__(self) #init Car object
+    def __init__(self, mapThisIsIn):
+        Map.Car.__init__(self, mapThisIsIn) #init Car object
         
         self.acceleration = 0.7 #m/s^2
         self.steer_accel = 1.0 #radians/s
@@ -20,7 +22,9 @@ class simCar(Map.Car):
         self.min_velocity = -1
         self.max_velocity = 3
         
-        self.lastSimUpdateTime = time.time()
+        self.mapThisIsIn = mapThisIsIn
+        
+        self.lastSimUpdateTime = self.mapThisIsIn.clock()
     
     def sendSpeedAngle(self, speed, angle):
         """set desired speed and steering angle
@@ -34,7 +38,7 @@ class simCar(Map.Car):
         """handle acceleration (both velocity and steering), to simulate getting sensor feedback from car
             (fake version of carMCU class)"""
         ## in the realCar class, speed and steering is updated in car.update(), but for the simulation we'll just update those here
-        rightNow = time.time()
+        rightNow = self.mapThisIsIn.clock()
         dt = rightNow - self.lastSimUpdateTime
         
         if(abs(self.desired_velocity - self.velocity) > abs((self.acceleration * dt)/2)): #if there's still room to improve
