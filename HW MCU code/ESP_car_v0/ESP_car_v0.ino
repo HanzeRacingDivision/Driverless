1cafe6d1cc8f7a3169c1de88fdac72ff9e14aff6
Thijs van Liempd <33570639+thijses@users.noreply.github.com>
2021-04-06 18:39:12 +0200

    added significant calibration improvements
    steering servo control calibration (to be improved, but 90%+ accurate)
    steering adds resistance that needs to be counteracted with motor power, this update adds several options for doing that
    also a few minor bugfixes. floats that should be integers, silly mistakes.
    TBD: battery motor calibration, better allround calibration accuracy

--- HW MCU code/ESP_car_v0/ESP_car_v0.ino
+++ HW MCU code/ESP_car_v0/ESP_car_v0.ino
@@ -1,5 +1,5 @@
-#define useBluetooth
-#define usePWM
+//#define useBluetooth
+//#define usePWM
 
 #ifdef useBluetooth
   #include "BluetoothSerial.h"
@@ -8,6 +8,7 @@
   #define theSerial SerialBT
 #else
   #define theSerial Serial
+  //#define theSerial Serial2
 #endif
 
 #ifdef usePWM
@@ -20,18 +21,44 @@
   const uint32_t PWMnoSignalTimeout = 1000000; //if there's no PWM signal for this long
 #endif
 
+#define abs(x) ((x)>0?(x):-(x)) //makes abs() work with floats
 
 const float minTargetSpeed = 0.7; //can't go slower than this
 float targetSpeed; //target speed in m/s
 int16_t servoTarget; //target steering servo pulselength (initialized to servoMid at setup())
 
+#define expKpSteer
+#define expKpSteerZeroing
+
 //super simple control system for now, PID to come at some point
-// output = (targetSpeed * motorKpBase) + ((targetSpeed - mesSpeed) * motorKpAdj)
-const float motorKpBase = 19.0; //motorKpBase will be based on rudimentary calibration (hopefully linear)
-const float motorKpAdj = 15.0; //motorKpAdj should be small to reduce overshoot, but big enough to prevent underperforming steady-states (at very low/high speeds)
-const float motorKpAdjAvgDepth = 4; //number of points to consider when calculating average speed (MUST BE <= speedLogFIFOsize)
 const uint32_t motorControlInterval = 100; //time between control system updates in millis. The shorter this is, the more aggresive motorKpAdj will be used
 uint32_t motorControlTimer;
+// output = (targetSpeed * motorKpBase) + ((targetSpeed - mesSpeed) * motorKpAdj) + some other things
+const float motorKpBase = 19.0; //motorKpBase will be based on rudimentary calibration (hopefully linear)
+const float motorKpAdj = 30.0; //motorKpAdj should be small to reduce overshoot, but big enough to prevent underperforming steady-states (at very low/high speeds)
+const uint8_t motorKpAdjAvgDepth = 4; //number of points to consider when calculating average speed (MUST BE <= speedLogFIFOsize)
+const float motorKpSteerBase = 45.0; //the effects of steering (resistance) are unknown, but can be calibrated by changing this number
+const uint8_t motorKpSteerAvgDepth = 4; //number of points to consider when calculating average steering angle (MUST BE <= servoLogFIFOsize)
+#ifdef expKpSteer //the steering resistance curve can be approximated with an exponential curve (at the cost of ~20/25 microseconds per calculation)
+  //on the ESP32, exp() takes around 10 microseconds (according to my own quick tests)
+  const float motorKpSteerMult[2] = {0.017974*motorKpSteerBase, 0.063663*motorKpSteerBase}; //left and right turns behanve differently (dont ask me why)
+  const float motorKpSteerExp[2] = {-0.115, 0.0677};
+  #ifdef expKpSteerZeroing //a (linear) transition between no compensation and max compensation
+    const float motorKpSteerMax[2] = {-25, 25}; //degrees
+    const float motorKpSteerMultTwo[2] = {motorKpSteerMult[0]/motorKpSteerMax[0], motorKpSteerMult[1]/motorKpSteerMax[1]};
+    //output += motorKpSteerMult*exp(motorKpSteerExp*steerAngle) - ((motorKpSteerMax-steerAngle)*motorKpSteerMultTwo)
+    //without this, output=motorKpSteerMult if steerAngle=0
+    //this just adds a linear function to stretch the output from (motorKpSteerMult, maxKp) to (0, maxKp)
+  #else
+    //note: this will have a hard transition between no compensation and some compensation, which may induce speed oscillation
+    const float motorKpSteerMin[2] = {-5, 5}; //degrees (exponential output is not 0 at 0 steerAngle (input), so any steerAngle less than this does not affect motor
+    //output += motorKpSteerMult*exp(motorKpSteerExp*steerAngle)   IF steerAngle >/< min
+  #endif
+#else
+  const float motorKpSteerMult[2] = {-0.0216*motorKpSteerBase, 0.0177*motorKpSteerBase};//output += (steerAngle-motorKpSteerOffset)*motorKpSteerMult
+  const float motorKpSteerOffset[2] = {(0.232332*motorKpSteerBase)/motorKpSteerMult[0], (0.097332*motorKpSteerBase)/motorKpSteerMult[1]};
+#endif
+//const float motorKpVolt = ; //adjustment based on battery voltage, lower voltage -> higher motor power (compensating)
 
 //let's make a fun little First In First Out speed logger, to keep track of (short-term) speed changes (to debug control loop stability)
 const uint8_t speedLogFIFOsize = 8; //size of speedLog
@@ -129,10 +156,12 @@ int16_t avgServo(uint8_t depth = servoLogFIFOsize) {
 #define servoMin 500  //servo minimum pulse length (PWM)
 #define servoMax 2400
 static const int16_t servoMid = (servoMax - servoMin)/2 + servoMin + (servoMidOffset); //(output) center value (straight steering)
-static const float servoMicrosPerDegree = 20.0; //(output) microseconds per degree (TO BE CALIBRATED)
+static const float servoMicrosPerDegree[2] = {28.948, 23.283}; //(output) microseconds per degree
+static const float servoMicrosPerDegreeOffset[2] = {-0.106, 0.028}; //servoPWM = servoMid + ((degrees - offset) * microsPerDegree))
 #define servoPWMChan 0
-static const float servoADCvalPerDegree = 20.0; //(input) ADCval per degree (TO BE CALIBRATED)
-static const int16_t servoPotMid = 2048; //(input) center value (straight steering) for ADC data from servo potentiometer
+static const float servoADCvalPerDegree[2] = {49.634, 40.494}; //(input) ADCval per degree
+static const int16_t servoPotMid = 2025; //(input) center value (straight steering) for ADC data from servo potentiometer
+static const int16_t servoADCvalPerDegreeOffset[2] = {servoPotMid-6, servoPotMid+3}; //degrees = (ADCval - offset) / ADCvalPerDegree
 
 static const int16_t motorForwStart = 1469; //forward range (calibrated)
 static const int16_t motorBackwStart = 1550; //backwards range (to be calibrated)
@@ -141,6 +170,59 @@ static const int16_t motorForwRange = motorForwStart-servoMin; //forward range i
 static const int16_t motorBackwRange = servoMax-motorBackwStart; //backwards range is from 1550 to 2400
 #define motorPWMChan 1
 
+
+int16_t degreesToServoPWM(float degreeInput) { //convert from degrees to servo PWM pulselength
+  //degreeInput = constrain(degreeInput, serialInputAngleMin, serialInputAngleMax) //already done before function is called
+  if(degreeInput < servoMicrosPerDegreeOffset[0]) {
+    return(servoMid + ((int16_t)((degreeInput-servoMicrosPerDegreeOffset[0]) * servoMicrosPerDegree[0])));
+  } else if(degreeInput > servoMicrosPerDegreeOffset[1]) {
+    return(servoMid + ((int16_t)((degreeInput-servoMicrosPerDegreeOffset[1]) * servoMicrosPerDegree[1])));
+  } else {
+    return(servoMid);
+  }
+}
+
+float ADCvalToDegrees(int16_t ADCvalInput) { //convert from (servo pot) ADCval to degrees
+  if(ADCvalInput < servoADCvalPerDegreeOffset[0]) {
+    return(((float)(ADCvalInput - servoADCvalPerDegreeOffset[0]))/servoADCvalPerDegree[0]);
+  } else if(ADCvalInput > servoADCvalPerDegreeOffset[1]) {
+    return(((float)(ADCvalInput - servoADCvalPerDegreeOffset[1]))/servoADCvalPerDegree[1]);
+  } else {
+    return(0.0);
+  }
+}
+
+float motorKpSteer(uint8_t depth = servoLogFIFOsize) {
+  float steerVal = ADCvalToDegrees(avgServo(depth));
+  #ifdef expKpSteer
+    #ifdef expKpSteerZeroing
+      if(steerVal < 0) {
+        return(motorKpSteerMult[0]*exp(motorKpSteerExp[0]*steerVal) - ((motorKpSteerMax[0]-steerVal)*motorKpSteerMultTwo[0]));
+      } else if(steerVal > 0) {
+        return(motorKpSteerMult[1]*exp(motorKpSteerExp[1]*steerVal) - ((motorKpSteerMax[1]-steerVal)*motorKpSteerMultTwo[1]));
+      }
+    #else
+      if(steerVal < motorKpSteerMin) {
+        return(motorKpSteerMult[0]*exp(motorKpSteerExp[0]*steerVal));
+      } else if(steerVal > motorKpSteerMin) {
+        return(motorKpSteerMult[1]*exp(motorKpSteerExp[1]*steerVal));
+      }
+    #endif
+  #else
+    if(steerVal < motorKpSteerOffset[0]) {
+      return((steerVal-motorKpSteerOffset[0])*motorKpSteerMult[0]);
+    } else if(steerVal > motorKpSteerOffset[1]) {
+      return((steerVal-motorKpSteerOffset[1])*motorKpSteerMult[1]);
+    }
+  #endif
+  else {
+    return(0.0);
+  }
+}
+
+//float motorKpVolt() {
+//  TBD
+//}
 
 //the main PC will send desired speed + steering angle in the following format:
 //speed steeringAngle\n
@@ -153,8 +235,8 @@ const uint32_t serialInReadTimeout = 5000; //(micros) if reading all the bytes i
 const char serialInputSeperator = ' '; //seperator between data
 const float serialInputSpeedMin = -3.1; //minimum allowed speed input
 const float serialInputSpeedMax = 5.1; //maximum allowed speed input
-const float serialInputAngleMin = -45.1; //minimum allowed angle input
-const float serialInputAngleMax = 45.1; //maximum allowed angle input
+const float serialInputAngleMin = -25.1; //minimum allowed angle input
+const float serialInputAngleMax = 25.1; //maximum allowed angle input
 
 //the MCU must send sensor feedback to main PC in the following format:
 //speed distance_driven_since_last_time steeringAngle\n
@@ -162,8 +244,9 @@ const float serialInputAngleMax = 45.1; //maximum allowed angle input
 //1.23 0.89 15.0
 //units: speed in m/s, 2 decimals  ;  distance in meters, 2 decimals  ;  angle in degrees, 1 decimal
 uint32_t serialFeedbackTimer;
-const uint32_t serialFeedbackInterval = 300; //time between serial feedback messages (in millis)
-const float serialFeedbackAvgSpeedDepth = 4; //number of points to consider when calculating average speed (MUST BE <= speedLogFIFOsize)
+const uint32_t serialFeedbackInterval = 100; //time between serial feedback messages (in millis)
+const uint8_t serialFeedbackAvgSpeedDepth = 2; //number of points to consider when calculating average speed (MUST BE <= speedLogFIFOsize)
+const uint8_t serialFeedbackAvgSteerDepth = 2; //number of points to consider when calculating average speed (MUST BE <= servoLogFIFOsize)
 const char serialFeedbackSeperator = ' '; //seperator between data
 uint32_t serialFeedbackEncoCountLast; //holds encoder count from last time timer was triggered
 
@@ -187,7 +270,7 @@ void setup() {
   pinMode(servoPotPin, INPUT); //basically all pins are input by default, but just for safety
   pinMode(motorPin, OUTPUT); //probably also part of ledcAttachPin() but just for safety
   
-  ledcSetup(servoPWMChan, 61, 14); // at ~61Hz and 14bit res, the duty cycle value = microseconds
+  ledcSetup(servoPWMChan, 244, 12); // at ~244Hz and 12bit res, the duty cycle value = microseconds
   ledcAttachPin(servoPin, servoPWMChan);
   ledcSetup(motorPWMChan, 61, 14); // at ~61Hz and 14bit res, the duty cycle value = microseconds
   ledcAttachPin(motorPin, motorPWMChan);
@@ -247,7 +330,7 @@ void loop() {
       String angleString = serialInWholeString.substring(serialInputSeperatorLocationLast, serialInputSeperatorLocation); //get substring up to (not including) seperator
       float serialInputAngle = angleString.toFloat();
       if((serialInputAngle > serialInputAngleMin) && (serialInputAngle < serialInputAngleMax)) {
-        servoTarget = servoMid + ((int16_t)(serialInputAngle * servoMicrosPerDegree)); //convert from degrees to servo PWM pulselength
+        servoTarget = degreesToServoPWM(serialInputAngle);
       } //else { Serial.println("bad message, couldn't parse angle"); }
       #ifdef usePWM
         if(!serialControlOverride) {
@@ -291,7 +374,10 @@ void loop() {
     
     if(targetSpeed > minTargetSpeed) { //this check is done here (as opposed to when receiving the targetspeed input/command) because there already needs to be an if-statement anyways, motorForwStart != motorMid
       float avgMesSpeed = avgSpeed(motorKpAdjAvgDepth); //get average speed (default is full depth, otherwise: avgSpeed(depth))
-      float output = (targetSpeed * motorKpBase) + ((targetSpeed - avgMesSpeed) * motorKpAdj);
+      float output = (targetSpeed * motorKpBase); //base multiplier
+      output += ((targetSpeed - avgMesSpeed) * motorKpAdj); //speed error multiplier
+      output += motorKpSteer(motorKpSteerAvgDepth) * targetSpeed; //steering adds resistance, this counteracts that (calibration was done at 1.0m/s target speed, idk if i need to multiply by speed yet, TBD)
+      //output += motorKpVolt() * targetSpeed; //lower battery voltage -> less power, this counteracts that  TBD
       ledcWrite(motorPWMChan, motorForwStart - constrain(((int16_t)output), -20, motorForwRange)); //shouldn't come out to illigal values (outside of 500-2400us)
     } else if(targetSpeed < -0.01) { // braking (doing "<0" is not quite 100% safe when using floats, but it may have been fine here)
       ledcWrite(motorPWMChan, motorMid); //temporary, braking/backwards math TBD
@@ -314,11 +400,11 @@ void loop() {
   if(millis() >= serialFeedbackTimer) {
     serialFeedbackTimer = millis() + serialFeedbackInterval;
     
-    float steeringAngle = ((float)(avgServo() - servoPotMid))/servoADCvalPerDegree; //convert from ADCval to degrees
-    float serialFeedbackDrvnDist = (encoCount - serialFeedbackEncoCountLast) * encoCountPerDM; //determine distance driven since last feedback
+    float steeringAngle = ADCvalToDegrees(avgServo(serialFeedbackAvgSteerDepth));
+    float serialFeedbackDrvnDist = (encoCount - serialFeedbackEncoCountLast) * encoCountPerM; //determine distance driven since last feedback
     serialFeedbackEncoCountLast = encoCount; //do this immedietly after, becuase encoCount is incremented via interrupts, so making/sending the string first could lead to skipped counts
     
-    String dataString = String(avgSpeed(serialFeedbackAvgSpeedDepth) * 10.0, 2) + serialFeedbackSeperator + String(serialFeedbackDrvnDist, 2) + serialFeedbackSeperator + String(steeringAngle, 1);
+    String dataString = String(avgSpeed(serialFeedbackAvgSpeedDepth), 2) + serialFeedbackSeperator + String(serialFeedbackDrvnDist, 2) + serialFeedbackSeperator + String(steeringAngle, 1);
     theSerial.println(dataString); //you could save 1 line of code by skipping the string init, but arduino doesn't want stuff in println()
   }
 }
