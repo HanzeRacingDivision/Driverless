34857897272ca5a2a5645c63a5ad25669d271af7
Michal Tesnar <michal.tesnar007@gmail.com>
2022-06-16 08:38:27 +0200

    Changing the speed of time
    Now it is possible to change the speed with which the clock operates to make the simulation run faster or slower.

--- processing/simulation/clock.py
+++ processing/simulation/clock.py
@@ -4,25 +4,40 @@ import time
 class Clock():
     def __init__(self):
         self.dt = 0
-        self.last_non_zero_dt = 0
-        self.time_previous = 0
+        self.real_dt = 0
+        self.real_time_previous = 0
         self.time_start = time.time()
-        self.time_running = time.time()
+        self.real_time_running = 0  # actual time from the start of the simulation in seconds
+        self.time_running = 0  # time from the start of the simulation with change of speed accounted for
+        self.speed = 1
 
     def update(self):
-        self.time_running = time.time() - self.time_start
-        self.dt = self.time_running - self.time_previous
-        self.time_previous = self.time_running
+        self.real_time_running = time.time() - self.time_start
+        self.real_dt = self.real_time_running - self.real_time_previous
+        self.dt = self.real_dt * self.speed
+        self.time_running += self.dt
+        self.real_time_previous = self.real_time_running
 
     def reset(self):
         self.dt = 0
-        self.last_non_zero_dt = 0
-        self.time_previous = 0
+        self.real_dt = 0
+        self.real_time_previous = 0
         self.time_start = time.time()
-        self.time_running = time.time()
+        self.real_time_running = 0
+        self.time_running = 0
+        self.speed = 1
+
+    def change_speed(self, new_speed):
+        self.speed = new_speed
 
     def get_dt(self):
         return self.dt
 
     def get_time_running(self):
         return self.time_running
+
+    def get_real_time_running(self):
+        return self.real_time_running
+
+    def get_speed(self):
+        return self.speed
