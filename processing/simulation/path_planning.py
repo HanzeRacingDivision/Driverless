99d3320909349c6f0db5e06acb5599cd7f516544
Michal Tesnar <michal.tesnar007@gmail.com>
2022-06-26 22:16:31 +0200

    Solving the nose bug

--- processing/simulation/path_planning.py
+++ processing/simulation/path_planning.py
@@ -189,9 +189,9 @@ class PathPlanning:
             observation[12 + 2 * i] = np.interp(cone[0], [0, CAR_FIELD_OF_VIEW / PIXELS_PER_UNIT], [-1, 1])
             observation[13 + 2 * i] = np.interp(cone[1], [-1 * CAR_FOV_RANGE, CAR_FOV_RANGE], [-1, 1])
 
-        # add noise -- not working
-        # noise = np.float32(np.random.normal(1, noise_scale, size=num_obs))
-        # observation = np.multiply(observation, noise)
+        # add noise
+        noise = np.float32(np.random.normal(1, noise_scale, size=num_obs))
+        observation = np.clip(np.multiply(observation, noise), -1, 1)
         return observation
 
     def run(self, method):
