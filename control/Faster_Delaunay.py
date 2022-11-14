4bbf93b8c0e5edcf4a9391fd4fa05cde1a15700d
scinii <roberto.schinina02@gmail.com>
2022-11-14 15:01:48 +0100

    Update Faster_Delaunay.py

--- control/Faster_Delaunay.py
+++ control/Faster_Delaunay.py
@@ -4,9 +4,8 @@ from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction import DictVectorizer
 
-#start_time = time.time()
 def separating(coordinates): 
-    
+
     coordinates.sort(key=lambda x:(x["Label"], x["Ypos"]**2+x["Xpos"]**2))
     dictvectorizer = DictVectorizer(dtype = np.float64 ,sparse= False)
     features = dictvectorizer.fit_transform(coordinates)
@@ -14,18 +13,14 @@ def separating(coordinates):
     return final_data
 
 example = [{"Label": "Blue", "Zpos": 2, "Ypos": 3, "Xpos": 7 , "Time": 1}, {"Label": "Yellow", "Zpos": 2, "Ypos": 3.83, "Xpos": 8.26 , "Time": 1}, {"Label": "Yellow", "Zpos": 2, "Ypos": 5.15, "Xpos": 6.82 , "Time": 1}, {"Label": "Blue", "Zpos": 2, "Ypos": 4, "Xpos":6 , "Time": 1}, {"Label": "Blue", "Zpos": 2, "Ypos": 4.49, "Xpos": 4.6 , "Time": 1}, {"Label": "Yellow", "Zpos": 2, "Ypos": 5.77, "Xpos": 5.18 , "Time": 1} ]
-
-
 example.sort(key=lambda x:(x["Label"], x["Ypos"]**2+x["Xpos"]**2))
 output = separating(example) 
 tri = Delaunay(output)
 final_triangles = tri.simplices
-n = int(np.shape(final_triangles[0]))
+n = int(np.shape(final_triangles)[0])
 for i in range(n):
     if example[tri.simplices[i][0]]["Label"] == example[tri.simplices[i][1]]["Label"] == example[tri.simplices[i][2]]["Label"]:
         final_triangles = np.delete(final_triangles,i,0) 
 plt.triplot(output[:,0], output[:,1], final_triangles)
 plt.plot(output[:,0], output[:,1], 'o')
 plt.show()
-#final_time = time.time()-start_time
-#print(final_time)
