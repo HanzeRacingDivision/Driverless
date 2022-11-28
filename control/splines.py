9e4c21e093a8f5534d4804768a7e86f9b571dc60
BlueJ1 <lenny103@arcor.de>
2022-11-28 15:26:18 +0100

    Added a bit of documentation, renamed function and simplified it

--- control/splines.py
+++ control/splines.py
@@ -2,19 +2,33 @@ from scipy.interpolate import splprep, splev
 import numpy as np
 
 
-def generate_spline(midpoints: np.ndarray, distance_increment: float = 0.05):
-    midpoints = np.concatenate([[[0, 0]], midpoints])
-    if midpoints.shape[0] >= 3:
-        midpoints = midpoints[:3]
+def generate_increment_on_path(midpoints: np.ndarray, distance_increment: float = 0.05,
+                               max_midpoints_considered: int = 3):
+    """
+    This function takes an array of midpoints and generates a new point along the path, described by these midpoints.
+
+    # TODO Do we maybe need an absolute increment (or one dependent on our speed)?
+    Currently, our 'step size' is dependent on how far the last midpoint we consider, is away.
+
+    :param midpoints:
+    :param distance_increment: float
+    :param max_midpoints_considered: int
+    :return:
+    """
+
+    if midpoints.shape[0] >= 2:
+        # we only care about the first few midpoints to fit our line, and we add our current position to the array
+        midpoints = np.concatenate([[[0, 0]], midpoints[: max_midpoints_considered]])
 
         mytck, myu = splprep([midpoints[:, 0], midpoints[:, 1]])
         xnew, ynew = splev([distance_increment], mytck)
         xnew, ynew = xnew[0], ynew[0]
-    elif midpoints.shape[0] == 2:
-        x1, y1, x2, y2 = midpoints[0][0], midpoints[0][1], midpoints[1][0], midpoints[1][1]
-        xnew = x1 + (x2 - x1) * distance_increment
-        ynew = y1 + (y2 - y1) * distance_increment
+    elif midpoints.shape[0] == 1:
+        # we move an incremental step on the line towards the point
+        x, y = midpoints[1][0], midpoints[1][1]
+        xnew = x * distance_increment
+        ynew = y * distance_increment
     else:
-        xnew, ynew = distance_increment, distance_increment
+        xnew, ynew = distance_increment, 0  # move straight
 
     return np.array([xnew, ynew])
