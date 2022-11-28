26196e1d2eb21f58424b65876dc5e660560ca5ed
BlueJ1 <lenny103@arcor.de>
2022-11-28 15:01:04 +0100

    Implemented spline generation for any number of visible midpoints

--- control/splines.py
+++ control/splines.py
@@ -11,7 +11,9 @@ def generate_spline(midpoints: np.ndarray, distance_increment: float = 0.05):
         xnew, ynew = splev([distance_increment], mytck)
         xnew, ynew = xnew[0], ynew[0]
     elif midpoints.shape[0] == 2:
-        ...
+        x1, y1, x2, y2 = midpoints[0][0], midpoints[0][1], midpoints[1][0], midpoints[1][1]
+        xnew = x1 + (x2 - x1) * distance_increment
+        ynew = y1 + (y2 - y1) * distance_increment
     else:
         xnew, ynew = distance_increment, distance_increment
 
