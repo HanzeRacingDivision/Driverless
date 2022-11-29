986d81eef3556e2501161283f872f12c1fc40798
EneRgYCZ <catalin.zaharia2013@gmail.com>
2022-11-29 10:02:03 +0100

    Updates to solve merging conflicts V3 (/control)

--- control/Faster_Delaunay.py
+++ control/Faster_Delaunay.py
@@ -3,20 +3,23 @@ from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction import DictVectorizer
 
-def separating(coordinates): 
 
-    coordinates.sort(key=lambda x:(x["Label"], x["Ypos"]**2+x["Xpos"]**2))
-    dictvectorizer = DictVectorizer(dtype = np.float64 ,sparse= False)
+def separating(coordinates):
+    # coordinates.sort(key=lambda x:(x["Label"], x["Ypos"]**2+x["Xpos"]**2))
+    dictvectorizer = DictVectorizer(dtype=np.float64, sparse=False)
     features = dictvectorizer.fit_transform(coordinates)
-    final_data = np.delete(features,(0,1,2,5),axis = 1)
+    final_data = np.delete(features, (0, 1, 2, 5), axis=1)
     return final_data
 
+
 def delauney_boundary(coordinates):
-    coordinates.sort(key=lambda x:(x["Label"], x["Ypos"]**2+x["Xpos"]**2))
-    output = separating(coordinates) 
+    coordinates.sort(key=lambda x: (x["Label"], x["Ypos"]**2+x["Xpos"]**2))
+    output = separating(coordinates)
     tri = Delaunay(output)
     final_triangles = tri.simplices
     n = int(np.shape(final_triangles)[0])
     for i in range(n):
         if coordinates[tri.simplices[i][0]]["Label"] == coordinates[tri.simplices[i][1]]["Label"] == coordinates[tri.simplices[i][2]]["Label"]:
-            final_triangles = np.delete(final_triangles,i,0) 
+            final_triangles = np.delete(final_triangles, i, 0)
+
+    return final_triangles, coordinates
