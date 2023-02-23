a3a59f6df14c99ac2fa9e83717c701cfe1564060
LauraMQuiros <101637439+LauraMQuiros@users.noreply.github.com>
2023-02-23 18:11:40 +0100

    added file, commented out stuff

--- Hammad/Kalman-Filter-Example-master/Kalman-Filter-Example-master/Assignment/GPSIMUSensorFusion1.py
+++ Hammad/Kalman-Filter-Example-master/Kalman-Filter-Example-master/Assignment/GPSIMUSensorFusion1.py
@@ -8,16 +8,21 @@ from statistics import mean
 from madgwickahrs import MadgwickAHRS
 
 # Notation used coming from: https://www.bzarg.com/p/how-a-kalman-filter-works-in-pictures/
+# Not changed (look at notation link)
 def prediction(X_hat_t_1, P_t_1, F_t, B_t, U_t, Q_t):
     X_hat_t = F_t.dot(X_hat_t_1) + (B_t.dot(U_t))
     P_t = np.diag(np.diag(F_t.dot(P_t_1).dot(F_t.transpose()))) + Q_t
     return X_hat_t, P_t
+
+#Not changed (look at notation link)
 def update(X_hat_t, P_t, Z_t, R_t, H_t):
     K_prime = P_t.dot(H_t.transpose()).dot(np.linalg.inv(H_t.dot(P_t).dot(H_t.transpose()) + R_t))
     X_t = X_hat_t + K_prime.dot(Z_t - H_t.dot(X_hat_t))
     P_t = P_t - K_prime.dot(H_t).dot(P_t)
     return X_t, P_t
-def rotaitonMatrix(heading, attitude, bank):
+
+#Not changed (look at notation link)
+def rotationMatrix(heading, attitude, bank):
     '''
     :returns: rotation array in numpy format
     [m00 m01 m02]
@@ -40,6 +45,7 @@ def rotaitonMatrix(heading, attitude, bank):
     m21 = sh * sa * cb + ch * sb
     m22 = -sh * sa * sb + ch * cb
     return np.array([[m00, m01, m02], [m10, m11, m12], [m20, m21, m22]])
+
 def getDistance(lat1, lon1, lat2, lon2):
     '''
     refernce: http://code.activestate.com/recipes/577594-gps-distance-and-bearing-between-two-gps-points/
@@ -55,6 +61,7 @@ def getDistance(lat1, lon1, lat2, lon2):
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = R * c
     return distance * 1000.0
+
 def getBearing(lat1,lon1,lat2,lon2):
     '''
     reference: http://code.activestate.com/recipes/577594-gps-distance-and-bearing-between-two-gps-points/
@@ -67,58 +74,17 @@ def getBearing(lat1,lon1,lat2,lon2):
 
 samples = 30000
 
-# Defining file locations
-gpsDatafileName = "sensor_data/gpsValues.txt"
-accDatafileName = "sensor_data/accelerometerValues.txt"
-gyroDatafileName = "sensor_data/gyroscopeValues.txt"
-magDatafileName = "sensor_data/magneticValues.txt"
+# DataFileName and Values File Handles out
 
-# defining file handles
-gpsValuesFileHandle = open(gpsDatafileName, 'r')
-accValuesFileHandle = open(accDatafileName, 'r')
-gyroValuesFileHandle = open(gyroDatafileName, 'r')
-magValuesFileHandle = open(magDatafileName, 'r')
-
-# Initializing lists to hold data
+#  No idea why data or timestamp are here. These are empty lists
 GPSDATA, gpsTimeStamp, gpsLatitude, gpsLongitude, gpsAltitude, gpsBearing, gpsSpeed = ([] for i in range(7))
 ACCDATA, accTimeStamp, accX, accY, accZ = ([] for i in range(5))
 GYRODATA, gyroTimeStamp, gyroX, gyroY, gyroZ = ([] for i in range(5))
 MAGDATA, magTimeStamp, magX, magY, magZ = ([] for i in range(5))
 
 print("Reading files...")
-# reading text files
-for gpsData in gpsValuesFileHandle:
-    gpsData = gpsData.rstrip()
-    GPSDATA.append(gpsData.split(','))
-for i in range(6, len(GPSDATA)):  # ignoring erronious data points
-    gpsTimeStamp.append(float(GPSDATA[i][0]))
-    gpsLatitude.append(float(GPSDATA[i][1]))
-    gpsLongitude.append(float(GPSDATA[i][2]))
-    gpsBearing.append(float(GPSDATA[i][4]))
-for accData in accValuesFileHandle:
-    accData = accData.rstrip()
-    ACCDATA.append(accData.split(','))
-for i in range(1, len(ACCDATA)):
-    accTimeStamp.append(float(ACCDATA[i][0]))
-    accX.append(float(ACCDATA[i][1]))
-    accY.append(float(ACCDATA[i][2]))
-    accZ.append(float(ACCDATA[i][3]))
-for gyroData in gyroValuesFileHandle:
-    gyroData = gyroData.rstrip()
-    GYRODATA.append(gyroData.split(','))
-for i in range(1, len(GYRODATA)):
-    gyroTimeStamp.append(float(GYRODATA[i][0]))
-    gyroX.append(float(GYRODATA[i][1]))
-    gyroY.append(float(GYRODATA[i][2]))
-    gyroZ.append(float(GYRODATA[i][3]))
-for magData in magValuesFileHandle:
-    magData = magData.rstrip()
-    MAGDATA.append(magData.split(','))
-for i in range(1, len(MAGDATA)):
-    magTimeStamp.append(float(MAGDATA[i][0]))
-    magX.append(float(MAGDATA[i][1]))
-    magY.append(float(MAGDATA[i][2]))
-    magZ.append(float(MAGDATA[i][3]))
+# I assume we are making sensor fusion of each datapoint individually
+
 
 print("Interpolating...")
 # interpolating data to 30000 points
@@ -144,6 +110,7 @@ magXInterpolated = np.interp(magTimeStampInterp, magTimeStamp, magX)
 magYInterpolated = np.interp(magTimeStampInterp, magTimeStamp, magY)
 magZInterpolated = np.interp(magTimeStampInterp, magTimeStamp, magZ)
 
+# Is this a list of difference between next gps stamp - current * 0.000...01?? Bc if so which prediction are we making and for what
 dt = [(gpsTimeStampInterp[i + 1] - gpsTimeStampInterp[i]) * 0.000000001 for i in range(len(gpsTimeStampInterp) - 1)]
 dt.insert(0, 0)
 
@@ -151,7 +118,7 @@ accXAbsolute = []
 accYAbsolute = []
 
 print("Calculating absolute acc values...")
-heading = MadgwickAHRS(sampleperiod=mean(dt))
+heading = MadgwickAHRS(sampleperiod=mean(dt)) #getting the heading out of the mean gps values?
 for i in range(samples):
     gyroscope = [gyroZInterpolated[i], gyroYInterpolated[i], gyroXInterpolated[i]]
     accelerometer = [accZInterpolated[i], accYInterpolated[i], accXInterpolated[i]]
@@ -162,7 +129,7 @@ for i in range(samples):
     pitch = ahrs[1]
     yaw = ahrs[2] + (3.0 * (math.pi / 180.0))  # adding magenetic declination
     ACC = np.array([[accZ[i]], [accY[i]], [accX[i]]])
-    ACCABS = np.linalg.inv(rotaitonMatrix(yaw, pitch, roll)).dot(ACC)
+    ACCABS = np.linalg.inv(rotationMatrix(yaw, pitch, roll)).dot(ACC)
     accXAbsolute.append(-1 * ACCABS[0, 0])
     accYAbsolute.append(-1 * ACCABS[1, 0])
 
@@ -176,9 +143,9 @@ Q_t = np.array([[1000, 0, 100, 0], [0, 1000, 0, 100], [100, 0, 1000, 0], [0, 100
 B_t = np.array([[0.5 * dt[0] ** 2, 0, 0, 0], [0, 0.5 * dt[0] ** 2, 0, 0], [0, 0, dt[0], 0], [0, 0, 0, dt[0]]])
 # Control vector
 U_t = np.array([[accXAbsolute[0]], [accYAbsolute[0]], [accXAbsolute[0]], [accYAbsolute[0]]])
-# Measurment Matrix
+# Measurement Matrix
 H_t = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])
-# Measurment cov
+# Measurement cov
 R_t = np.identity(2)
 # Initial State
 X_hat_t = np.array([[gpsLongitudeInterpolated[0]], [gpsLatitudeInterpolated[0]], [0], [0]])
@@ -188,9 +155,11 @@ Yfilter = []
 
 print("Running kalman filter...")
 for i in range(len(dt)):
+    #we make prediction with IMU data
     X_hat_t, P_hat_t = prediction(X_hat_t, P_t, F_t, B_t, U_t, Q_t)  # STATE PREDICTION (IMU)
     Z_t = np.array([[gpsLongitudeInterpolated[i]], [gpsLatitudeInterpolated[i]]])
     Z_t = Z_t.reshape(Z_t.shape[0], -1)
+    #we update with prediction data, gps lon+lat, measurement cov, measurement matrix??
     X_t, P_t = update(X_hat_t, P_hat_t, Z_t, R_t, H_t)  # MEASUREMENT UPDATE (GPS)
     X_hat_t = X_t
     P_hat_t = P_t
@@ -200,6 +169,7 @@ for i in range(len(dt)):
     Xfilter.append(X_t[0, 0])
     Yfilter.append(X_t[1, 0])
 
+"""
 # reducing data points to 100 for plotting on google maps
 dtReduced = np.linspace(gpsTimeStamp[0], gpsTimeStamp[len(gpsTimeStamp) - 1], 100)
 XfilterInterp = np.interp(dtReduced, gpsTimeStampInterp, Xfilter)
@@ -211,6 +181,7 @@ gmap1.apikey = "AIzaSyDvvwPIEA8T9IUxPKaRZ6gp2f6xRtBYICU"
 gmap1.scatter(gpsLatitude, gpsLongitude, color='r', size=0.3, marker=False)
 gmap1.scatter(YfilterInterp, XfilterInterp, color='g', size=0.6, marker=False)
 gmap1.draw("results.html")
+"""
 
 print("Done...")
 
